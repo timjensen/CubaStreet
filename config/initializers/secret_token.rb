581fa3ae60cdb7b89dc85985177f7d534b3246fc
Tim Jensen <timdavidjensen@gmail.com>
2012-05-18 23:10:25 +1200

    Here we go again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CubaStreet::Application.config.secret_token = '790a5adda5f2111542cd7edf6c0bb7583d74f34db94b872ed443ccdd59da236ab8e30391e338d8d85a985bae12e9712f139372928e1c8db9231ceca80f8da440'
